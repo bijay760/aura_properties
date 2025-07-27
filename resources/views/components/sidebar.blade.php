8f7db45c344c4c3faf10beaa06afda2e626d3344
prop07 <rijalmanoj07@gmail.com>
2025-07-27 15:32:29 +0545

    :)

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,7 +1,7 @@
    <nav class="bg-neutral-800 p-4 text-white w-40 h-screen space-y-2">
       <p href="#" class="block p-2 hover:bg-gray-700 rounded">Dashboard</p>
        {{-- <a href="#" class="flex gap-2 p-2 hover:bg-gray-700 rounded"> {!! iconUser() !!}Profile</a> --}}
-         <a href="admin/users" class=" flex gap-2 p-2 hover:bg-gray-700 rounded">
+         <a href="/admin/users" class=" flex gap-2 p-2 hover:bg-gray-700 rounded">
              {!! iconUsers() !!}
 Users</a>
     <a href="#" class=" flex gap-2 p-2 hover:bg-gray-700 rounded">
